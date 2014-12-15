ee48b5b0f379131d8d5e5dc32f74a73c9853dc24
Massimiliano Marcon <me@marcon.me>
2014-12-15 08:06:56 +0100

    More content and more styling

--- _src/_macros/format.js
+++ _src/_macros/format.js
@@ -5,7 +5,17 @@ function multiline(array){
 }
 
 function paragraphs(array){
-    return '<p>' + array.map(transform).join('</p><p>') + '</p>';
+    return '<p>' + array.join('</p><p>') + '</p>';
+}
+
+function list(array, ordered){
+    var output = ordered ? '<ol>' : '<ul>';
+    output += '\n';
+    output += '<li>' + array.join('</li>\n<li>') + '</li>';
+    output += '\n';
+    output += ordered ? '</ol>' : '</ul>';
+
+    return output;
 }
 
 function md(string){
@@ -18,5 +28,6 @@ function md(string){
 module.exports = {
     multiline: multiline,
     paragraphs: paragraphs,
+    list: list,
     md: md
 };
